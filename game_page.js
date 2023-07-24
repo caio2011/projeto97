afeee313a51feb81ba2fa15cf4daaf0d55c1035b
caio2011 <126509711+caio2011@users.noreply.github.com>
2023-07-24 17:40:37 -0300

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,10 @@
+function send() {
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+}
+
+document.getElementById("output").innerHtMl = row;
+document.getElementById("number1").value = "";
+document.getElementById("number2").value = "";
+
